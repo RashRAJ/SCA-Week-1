a28ea18424d7fb3ac0e36a53bf0a85ba68baf2cd
Rasheedat atinuke jamiu <56790407+RashRAJ@users.noreply.github.com>
2020-10-05 16:40:32 +0100

    Update Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -4,8 +4,14 @@ import re
 
 length = int(input('Enter password length: '))
 if length < 6:
-    print('too weak, enter another length')
+    print('weak password')
     length = int(input('Enter password length: '))
+
+uc= int(input('enter no of upper case alphabets: '))
+lc= int(input('enter no of lower case alphabets: '))
+num= int(input('enter no of numbers in the password: '))
+char = int(input('enter no of charcaters in the password: '))
+
 
 #assigning characters
 capital_letters = string.ascii_lowercase
@@ -15,24 +21,14 @@ character = string.punctuation
 
 #password function
 def password_generator(length):
-    Generator = (character+numbers+small_letters+capital_letters)
-    password = random.choices(Generator, k=length)
-    password =  ''.join(password)
+    password1 = random.choices(capital_letters, k=uc)
+    password2= random.choices(small_letters, k=lc)
+    password3= random.choices(numbers, k=num)
+    password4= random.choices(character, k=char)
 
-    return password
+    password =(password1+password2+password3+password4)
+    random.shuffle(password)
+    password = ''.join(password)
+    print(password)
 
 password_generator(length)
-
-# password quality check    
-'''
-#To make sure there are alphabets, characters and numbers in the password
-        pattern = '(\w) (\W)'
-        match = re.search(pattern, password)
-
-
-        if match:
-            return password
-        else:
-            i += 1
-
-'''
