f5ee42675c35686d222a10576564792ad7aec595
Rasheedat atinuke jamiu <56790407+RashRAJ@users.noreply.github.com>
2020-10-05 12:13:15 +0100

    Add files via upload

---
+++ guess_a_num.py
@@ -0,0 +1,25 @@
+import random
+print('Welocome to guess a nubmber! \nToday might just be your lucky day, Come on take a guess ;)')
+print(          )
+print('='*8,'start','='*8)
+
+user_input = int(input('Enter a number between 0 - 20: '))
+lucky_number = random.randint(0, 21)
+
+
+
+def guess_a_num(user_input, lucky_number):
+    for i in range(6):
+        if lucky_number == user_input:
+            print('Congratulations you got the right number !!')
+            break
+        elif lucky_number > user_input:
+            print('Oops! Too low try again')
+            user_input = int(input('Enter a number between 0 - 20: '))
+        else:
+            print('Oops! Too high try again')
+            user_input = int(input('Enter a number between 0 - 20: '))
+
+       
+guess_a_num(user_input, lucky_number)
+print('oops!, you are out of turns :(')
